a67bd6ba8fc30d34f474db4b018a2f665fc7b953
Muhammad Junaid <m.junaidaqib@gmail.com>
2024-07-16 03:34:54 -0700

    password generator

--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -8,26 +8,61 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      {/* <nav className="bg-white shadow-md">
+      <div className="container mx-auto px-4 py-3 flex justify-between items-center">
+        <a href="/" className="text-2xl font-bold text-blue-500">
+          <img src="/logo.svg" alt="Good Hand Logo" className="h-8" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div className="flex items-center">
+          <div className="relative">
+          <button className="absolute right-0 top-0 mt-2 mr-3">
+              <svg
+                className="h-4 w-4 text-gray-500"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </button>
+            <input
+              type="text"
+              placeholder="Search courses here"
+              className="w-64 px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+          <button className="hover:bg-blue-700 text-black font-bold py-2 px-4 border rounded-md ml-4 w-40">
+            Login
+          </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </nav> */}
+    <header>
+  {/* <!-- Section: Split screen --> */}
+  <section className="container mx-auto">
+    {/* <!-- Grid --> */}
+    <div className="grid grid-cols-3">
+      {/* <!-- First column --> */}
+      <div className="bg-red-500"> Muhammd </div>
+      {/* <!-- First column --> */}
+
+      {/* <!-- Second column --> */}
+      <div className="bg-blue-500"> junaid </div>
+      {/* <!-- Second column --> */}
+
+      {/* <!-- Third column --> */}
+      <div className="bg-green-500"> aqib</div>
+      {/* <!-- Third column --> */}
+    </div>
+    {/* <!-- Grid --> */}
+  </section>
+  {/* <!-- Section: Split screen --> */}
+</header>
     </>
   )
 }
